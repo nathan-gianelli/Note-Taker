845218fa5c7bad4c6ab9f5913b647bf1e8664044
Nathan Gianelli <nathan.gianelli98@gmail.com>
2020-03-04 20:08:42 -0500

    new note

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const PORT = process.env.PORT || 8080;
+ 
+var saveNote = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,17 +16,37 @@ app.get("/", function(req, res) {
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname + "/public/notes.html"));
   });
+
   app.get("/api/notes", function(req, res) {
     fs.readFile('./db/db.json',"utf-8", (err, data) => {
         if(err) {
             throw err
-        } else {
-            res.json(data)
-            console.log(data)
-        }
-
+        } 
+         res.json(JSON.parse(data))
+            //console.log(data)
+        
     });
   });
+
+  
+app.post("/api/notes", (req,res) => {
+    let newNote = req.body;
+    let id = saveNote.length.toString();
+    newNote.id = id
+    saveNote.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(saveNote));
+    res.json(saveNote);
+})
+
+app.delete("/api/notes/", (req,res) => {
+    let note = req.body.id
+    saveNote =saveNote.filter(Element => {
+        return Element.id != note
+    })
+    fs.writeFileSync("./db/db.json", JSON.stringify(saveNote));
+    res.json(saveNote);
+})
+
 app.listen(PORT, function() {
   console.log('PORT 8080 is running')
 });
